3dddc58b03960a8be9f5e96591ad5a6395a8a580
spjuanjoc <juancho.312@hotmail.com>
2025-05-26 21:02:04 -0500

    feat: add nlohmann_json with conan 2.0

---
+++ json/README.md
@@ -0,0 +1,12 @@
+# nlohmann_json
+
+https://github.com/nlohmann/json
+
+
+```shell
+mkcd build
+conan install .. --build=missing --output-folder=.
+cmake .. -DCMAKE_BUILD_TYPE=Debug
+cmake --build . 
+./json_cpp 
+```
---
+++ json/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+#include <nlohmann/json.hpp>
+
+using namespace nlohmann;
+
+int
+main()
+{
+  json object = { "one",  "two", "three", "four", "five" };
+  json j2     = {
+                  { "pi", 3.141 },
+                  { "happy", true },
+                  { "name", "Niels" },
+                  { "nothing", nullptr },
+                  { "answer", { { "everything", 42 } } },
+                  { "list", { 1, 0, 2 } },
+                  { "object", { { "currency", "USD" }, { "value", 42.99 } } } };
+
+  std::cout << object.dump() << std::endl;
+  std::cout << j2.dump() << std::endl;
+  const auto ptr = j2["happy"].template get<bool>();
+  // const auto j3 = j2["list"];
+  // std::cout << j3 << std::endl;
+}
