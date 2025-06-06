85b956c6adaa6f9e5f657e09c9502b9da418f82c
spjuanjoc <juancho.312@hotmail.com>
2025-06-05 21:26:58 -0500

    feat: print missing, reformat

--- json/main.cpp
+++ json/main.cpp
@@ -7,19 +7,21 @@ using namespace nlohmann;
 int
 main()
 {
-  json object = { "one",  "two", "three", "four", "five" };
-  json j2     = {
-                  { "pi", 3.141 },
-                  { "happy", true },
-                  { "name", "Niels" },
-                  { "nothing", nullptr },
-                  { "answer", { { "everything", 42 } } },
-                  { "list", { 1, 0, 2 } },
-                  { "object", { { "currency", "USD" }, { "value", 42.99 } } } };
+  const json object = { "one", "two", "three", "four", "five" };
+  json       j2     = { { "pi", 3.141 },
+                        { "happy", true },
+                        { "name", "Niels" },
+                        { "nothing", nullptr },
+                        { "answer", { { "everything", 42 } } },
+                        { "list", { 1, 0, 2 } },
+                        { "object", { { "currency", "USD" }, { "value", 42.99 } } } };
 
   std::cout << object.dump() << std::endl;
   std::cout << j2.dump() << std::endl;
+
   const auto ptr = j2["happy"].template get<bool>();
-  // const auto j3 = j2["list"];
-  // std::cout << j3 << std::endl;
+  std::cout << std::boolalpha << ptr << std::endl;
+
+  const auto j3 = j2["list"];
+  std::cout << j3 << std::endl;
 }
